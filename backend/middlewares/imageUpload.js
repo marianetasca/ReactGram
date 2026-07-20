be34a89b110209e93e5dc1def7bde25e643530b1
Mariane <mariane012.12@gmail.com>
2026-07-20 18:22:35 -0300

    fix: aceita jpeg e webp no upload de imagens

--- backend/middlewares/imageUpload.js
+++ backend/middlewares/imageUpload.js
@@ -22,7 +22,7 @@ const imageStorage = multer.diskStorage({
 const imageUpload = multer({
   storage: imageStorage,
   fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(png|jpg)$/)) {
+    if (!file.originalname.match(/\.(png|jpg|jpeg|webp)$/)) {
       // uplaod only png and jpg formats
       return cb(new Error("Por favor, envie apenas png ou jpg!"));
     }
